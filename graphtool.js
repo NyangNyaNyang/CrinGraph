f7ab91bc28027a8517523879b46cf8a186f9daa3
dingo424 <intae4557@gmail.com>
2023-12-19 16:27:58 +0900

    Update graphtool.js

--- graphtool.js
+++ graphtool.js
@@ -1795,8 +1795,6 @@ function iso226(phon, targetFreq) {
       }
       const lowerLp = Lp[targetFreqRange.indexOf(lowerFreq)];
       const upperLp = Lp[targetFreqRange.indexOf(upperFreq)];
-      console.log(lowerLp);
-      console.log(upperLp);
       return interpolateLp(lowerFreq, lowerLp, upperFreq, upperLp, targetFreq);
     } else {
         // console.log(Lp[targetIndex]);
@@ -1832,14 +1830,15 @@ function loudness_equalizer(p, phon) {
     let boolType = false;
     if(!p.isTarget) {
         for(let i=0;i<p.rawChannels.length;i++) {
-            p.rawChannels[i].map((point) => [point[0], point[1] + iso226(phon, point[0]) - iso226(p.loudness, point[0])]);
+            for(let j=0;j<p.rawChannels[i].length;j++) {
+                p.rawChannels[i][j][1] = p.rawChannels[i][j][1] + iso226(phon, p.rawChannels[i][j][0]) - iso226(p.loudness, p.rawChannels[i][j][0]);
+            }
             console.log("done");
         }
     }
     else {
-        for(let i=0;i<p.rawChannels.length;i++) {
-            p.rawChannels[i].map((point) => [point[0], point[1] - iso226(phon, point[0]) + iso226(p.loudness, point[0])]);
-            console.log("done");
+        for(let j=0;j<p.rawChannels[i].length;j++) {
+            p.rawChannels[i][j][1] = p.rawChannels[i][j][1] - iso226(phon, p.rawChannels[i][j][0]) + iso226(p.loudness, p.rawChannels[i][j][0]);
         }
         boolType = true;
     }
