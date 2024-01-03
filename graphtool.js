c0487d68cc769d02b09167fc0a03543ee3d4e037
dingo424 <intae4557@gmail.com>
2024-01-03 17:36:11 +0900

    Update graphtool.js

--- graphtool.js
+++ graphtool.js
@@ -1267,6 +1267,7 @@ function updatePhoneTable() {
             loudness_equalizer(p, this.value);
         })
         .on("keypress", function(event) {
+            console.log(event);
             if (event.key === "Enter") {
               loudness_equalizer(p, this.value); // Call the function when Enter is pressed
             }
