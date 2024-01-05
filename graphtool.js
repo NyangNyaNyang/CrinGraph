bc614b59f5c373df0569868243b64db9165f14b1
dingo424 <intae4557@gmail.com>
2024-01-05 17:45:33 +0900

    Update graphtool.js

--- graphtool.js
+++ graphtool.js
@@ -1820,6 +1820,7 @@ function interpolateCubic(x, x0, y0, x1, y1, x2, y2, x3, y3) {
 
 function loudness_equalizer(p, phon) {
     console.log("loudness_equalizer triggered");
+    console.log(p);
     fn = p.fileName;
     if(p.isTarget) return;
 
