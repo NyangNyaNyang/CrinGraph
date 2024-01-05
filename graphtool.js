3c02eb6f621729da03700e94579f2aa3812660eb
dingo424 <intae4557@gmail.com>
2024-01-05 18:30:00 +0900

    Update graphtool.js

--- graphtool.js
+++ graphtool.js
@@ -1844,6 +1844,7 @@ function loudness_equalizer(p, phon) {
     }
 
     p.loudness[fn] = phon;
+    console.log(p,loudness[fn]);
     console.log(p);
     changeVariant(p, updateVariant);
     activeElem.focus();
